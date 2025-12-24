8509fa597d642504c2686412abae4c4234399f5d
Dalinda Djelassi <dalindadjelassi@MacBook-Air-de-Dalinda.local>
2025-12-24 19:32:12 +0100

    Correction api.js et affichage des films

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-import { getTrendingMovies, IMG_URL } from './api/tmdb.js';
+import { getTrendingMovies, IMG_URL } from './api.js';
 
-async function displayTrending() {
+async function displayMovies() {
     const movies = await getTrendingMovies();
     const container = document.getElementById('movie-container');
 
+    if (!movies) return;
+
     movies.forEach(movie => {
-        const card = document.createElement('div');
-        card.innerHTML = `
-            <img src="${IMG_URL}${movie.poster_path}" alt="${movie.title}">
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('movie-card'); // Pour le CSS plus tard
+        
+        movieCard.innerHTML = `
+            <img src="${IMG_URL}${movie.poster_path}" alt="${movie.title}" style="width:200px;">
             <h3>${movie.title}</h3>
             <p>Sortie : ${movie.release_date}</p>
             <a href="movie.html?id=${movie.id}">En savoir plus</a>
         `;
-        container.appendChild(card);
+        
+        container.appendChild(movieCard);
     });
 }
 
-displayTrending();
+displayMovies();
